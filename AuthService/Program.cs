08105b0c8fb031dca900a9a304a5d7962c49dbf7
artem <denbrown2003@gmail.com>
2020-08-08 18:08:54 +0300

    init

---
+++ AuthService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
