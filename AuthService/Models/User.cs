c170e929e87469adeb97d9d000e9ab9979eba1f6
artem <denbrown2003@gmail.com>
2020-08-08 19:48:56 +0300

    added class with User model

---
+++ AuthService/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthService.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public byte[] PasswordHash { get; set; }
+        [Required]
+        public int PasswordSalt { get; set; }
+
+
+    }
+}
