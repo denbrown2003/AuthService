327cd69540535b7eed7983e29f4d5d72391c88c3
artem <denbrown2003@gmail.com>
2020-08-09 14:27:34 +0300

    added startup class

---
+++ AuthService/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AuthService
+{
+    class Startup
+    {
+        private IConfigurationRoot Configuration { get; }
+        static private Startup obj = null;
+
+        private Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        static public IConfigurationRoot Config()
+        {
+            if (obj == null) obj = new Startup();
+            return obj.Configuration;
+        }
+    }
+}
