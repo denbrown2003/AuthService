682b98d1a9c905460f64cba5da471d7df16a1f5e
artem <denbrown2003@gmail.com>
2020-08-09 14:23:37 +0300

    delete old structure

---
+++ DockerApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace DockerApp
+{
+    class Program
+    {
+        static IConfigurationRoot config = Startup.Config();
+        static void Main(string[] args)
+        {
+         
+            string nn = config["MAIL_SERVER"];
+            string mode = config["ASPNETCORE_ENVIRONMENT"];
+            string test = config["TEST"];
+
+            Console.WriteLine(nn);
+            Console.WriteLine(mode);
+            Console.WriteLine(test);
+
+        }
+        
+
+    }
+
+
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+        static private Startup obj = null;
+
+        private Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        static public IConfigurationRoot Config()
+        {
+            if (obj == null)obj = new Startup();
+            return obj.Configuration;
+        }
+    }
+}
