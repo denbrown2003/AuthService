7bdb2cdf6c80eaff7a3eb2236bc6b4ab5dca6c62
artem <denbrown2003@gmail.com>
2020-08-10 15:05:14 +0300

    added logger. Graylog Connetcot

--- DockerApp/Program.cs
+++ DockerApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Appy.Logging.GrayLog;
+using Microsoft.Extensions.Configuration;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
@@ -10,18 +11,23 @@ namespace DockerApp
     class Program
     {
         static IConfigurationRoot config = Startup.Config();
+        static GrayLogUdpClient Logger = Startup.Logger;
         static void Main(string[] args)
         {
-            AppContext.GetContext();
+
+            /*AppContext.GetContext();
             Rabbit rabbit = new Rabbit();
 
             string queue = "demoqueue";
             var channel = rabbit.getChannel(queue);
 
-            MessageReceiver messageReceiver = new MessageReceiver(channel);
+            MessageReceiver messageReceiver = new MessageReceiver(channel);1
             channel.BasicConsume(queue, false, messageReceiver);
+            */
 
-            Console.ReadLine();
+            Logger.Send("Auth service is start up");
+
+
         }
     }
 }
--- DockerApp/Startup.cs
+++ DockerApp/Startup.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using DockerApp.models;
 using System;
+using Appy.Logging.GrayLog;
 
 namespace DockerApp
 {
@@ -9,14 +10,18 @@ namespace DockerApp
     {
         public IConfigurationRoot Configuration { get; }
         static private Startup obj = null;
-
+        public static GrayLogUdpClient Logger = null;
 
         private Startup()
         {
+ 
+          
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
+
+            Logger = new GrayLogUdpClient("AuthService", "192.168.0.16", 12201);
 
         }
 
@@ -25,5 +30,7 @@ namespace DockerApp
             if (obj == null) obj = new Startup();
             return obj.Configuration;
         }
+
+
     }
 }
